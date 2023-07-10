adca517ccd8120e86d9a3eab6e2c9c7a364e8735
Lokasku <lukasku@proton.me>
2023-07-11 00:06:26 +0200

    Add routes managment

--- app/Main.hs
+++ app/Main.hs
@@ -4,15 +4,11 @@ module Main where
 
 import Web.Twain as Twain
 import Network.Wai.Handler.Warp (run)
+import Route as R
 
 main :: IO ()
 main = do
     run 1024 myApp
 
-tree :: [Middleware]
-tree = [ get "/" $ send $ html "Welcome"
-       , get "/about" $ send $ html "<p>About me</p>"
-       ]
-
 myApp :: Application
-myApp = foldr ($) (notFound $ send $ html "<p>Error 404</p>") tree
+myApp = foldr ($) (notFound $ send $ html "<p>not found</p>") R.routes
---
+++ app/Route.hs
@@ -0,0 +1,36 @@
+{-# LANGUAGE OverloadedStrings #-}
+
+module Route where
+
+import Web.Twain
+import Data.ByteString.Lazy.Internal
+
+routes :: [Middleware]
+routes =
+    [ get "/" $ send $ html "<p>welcome</p>"
+    , get "/about" $ send $ html "<p>about me</p>"
+    , get "/:article" $ do
+        name <- pathParam "article"
+        case lookup name articles of
+            Nothing -> next
+            Just article -> send $ html article
+    ]
+
+articles :: [(FilePath, ByteString)]
+articles =
+    [ ("this-blog", "haha, just some blog, what a question")
+    ]
+
+{- genTree :: Routes -> Tree
+genTree = List.foldl (\ acc (p, c) ->
+                if List.notElem p (List.map fst tree)
+                then get p (send (html (c))) : acc
+                else get p (send (html (c))) : acc
+                ) []
+
+tree :: Routes
+tree = [ ("/", "Welcome")
+       , ("/about", "About me")
+       , ("/:article", "An article called : ")
+       ]
+-}
