e1d9eec0dac7cfb57b654577933b572612716c2c
Lokasku <lukasku@proton.me>
2023-07-10 12:34:22 +0200

    Add `/` and `about/`.

--- app/Main.hs
+++ app/Main.hs
@@ -1,4 +1,18 @@
+{-# LANGUAGE OverloadedStrings #-}
+
 module Main where
 
+import Web.Twain as Twain
+import Network.Wai.Handler.Warp (run)
+
 main :: IO ()
-main = putStrLn "Hello, Haskell!"
+main = do
+    run 1024 myApp
+
+tree :: [Middleware]
+tree = [ get "/" $ send $ html "Welcome"
+       , get "/about" $ send $ html "<p>About me</p>"
+       ]
+
+myApp :: Application
+myApp = foldr ($) (notFound $ send $ html "<p>Error 404</p>") tree
